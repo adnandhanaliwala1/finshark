c5ed8f00b38f0daffd4fea293ede14cc68cb643c
Adnan Dhanaliwala <dhanaliwalaai@vcu.edu>
2025-02-19 17:04:10 -0500

    search, api connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { ChangeEvent, SyntheticEvent, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Card from "./Components/Card";
+import CardList from "./CardList/CardList";
+import Search from "./Components/Search/Search";
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(e);
+  };
+  const onClick = (e: SyntheticEvent) => {
+    console.log(e);
+  };
+
   return (
     <div className="App">
-      <Card />
+      <Search onClick={onClick} search={search} handleChange={handleChange} />
+      <CardList />
     </div>
   );
 }
---
+++ frontend/src/Components/Search/Search.tsx
@@ -0,0 +1,22 @@
+import React, { ChangeEvent, JSX, useState, SyntheticEvent } from "react";
+
+interface Props {
+  onClick: (e: SyntheticEvent) => void;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  search: string | undefined;
+}
+
+const Search: React.FC<Props> = ({
+  onClick,
+  handleChange,
+  search,
+}: Props): JSX.Element => {
+  return (
+    <div>
+      <input value={search} onChange={(e) => handleChange(e)}></input>
+      <button onClick={(e) => onClick(e)} />
+    </div>
+  );
+};
+
+export default Search;
