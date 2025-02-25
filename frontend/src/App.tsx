e1faab0bc1df03fa290cfb07ed1164f53f8a6b14
Adnan Dhanaliwala <dhanaliwalaai@vcu.edu>
2025-02-25 15:26:22 -0500

    add tailwind and portfolio

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,67 @@
 import React, { ChangeEvent, SyntheticEvent, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import CardList from "./CardList/CardList";
+import CardList from "./Components/CardList/CardList";
 import Search from "./Components/Search/Search";
+import { CompanySearch } from "./company";
+import { searchCompanies } from "./api";
+import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
 
 function App() {
   const [search, setSearch] = useState<string>("");
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServorError] = useState<string>("");
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log(e);
   };
-  const onClick = (e: SyntheticEvent) => {
-    console.log(e);
+
+  const onPortfolioCreate = (e: any) => {
+    e.preventDefault();
+    const exists = portfolioValues.find((value) => value === e.target[0].value);
+    const updatedPortfolio = [...portfolioValues, e.target[0].value];
+    if (exists) return;
+    setPortfolioValues(updatedPortfolio);
+  };
+
+  const onPortfolioDelete = (e: any) => {
+    e.preventDefault();
+    const removed = portfolioValues.filter((value) => {
+      return value !== e.target[0].value;
+    });
+    setPortfolioValues(removed);
+  };
+
+  const onSearchSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const result = await searchCompanies(search);
+    if (typeof result === "string") {
+      setServorError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
+    console.log(searchResult);
   };
 
   return (
     <div className="App">
-      <Search onClick={onClick} search={search} handleChange={handleChange} />
-      <CardList />
+      <Navbar />
+      <Search
+        onSearchSubmit={onSearchSubmit}
+        search={search}
+        handleSearchChange={handleSearchChange}
+      />
+      {serverError && <h1>{serverError}</h1>}
+      <ListPortfolio
+        portfolioValues={portfolioValues}
+        onPortfolioDelete={onPortfolioDelete}
+      />
+      <CardList
+        searchResult={searchResult}
+        onPortfolioCreate={onPortfolioCreate}
+      />
     </div>
   );
 }
--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,21 +1,33 @@
 import React, { ChangeEvent, JSX, useState, SyntheticEvent } from "react";
 
 interface Props {
-  onClick: (e: SyntheticEvent) => void;
-  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onSearchSubmit: (e: SyntheticEvent) => void;
+  handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
   search: string | undefined;
 }
 
 const Search: React.FC<Props> = ({
-  onClick,
-  handleChange,
+  onSearchSubmit,
+  handleSearchChange,
   search,
 }: Props): JSX.Element => {
   return (
-    <div>
-      <input value={search} onChange={(e) => handleChange(e)}></input>
-      <button onClick={(e) => onClick(e)} />
-    </div>
+    <section className="relative bg-gray-100">
+      <div className="max-w-4xl mx-auto p-6 space-y-6">
+        <form
+          className="form relative flex flex-col w-full p-10 space-y-4 bg-darkBlue rounded-lg md:flex-row md:space-y-0 md:space-x-3"
+          onSubmit={onSearchSubmit}
+        >
+          <input
+            className="flex-1 p-3 border-2 rounded-lg placeholder-black focus:outline-none"
+            id="search-input"
+            placeholder="Search companies"
+            value={search}
+            onChange={handleSearchChange}
+          ></input>
+        </form>
+      </div>
+    </section>
   );
 };
 
