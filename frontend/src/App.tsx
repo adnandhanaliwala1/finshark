c2549294112e35d3e7c1fe6022482b91c59e790b
Adnan Dhanaliwala <dhanaliwalaai@vcu.edu>
2025-02-26 00:02:07 -0500

    routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,68 +1,14 @@
 import React, { ChangeEvent, SyntheticEvent, useState } from "react";
 import "./App.css";
-import CardList from "./Components/CardList/CardList";
-import Search from "./Components/Search/Search";
-import { CompanySearch } from "./company";
-import { searchCompanies } from "./api";
-import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
 import Navbar from "./Components/Navbar/Navbar";
-import Hero from "./Components/Hero/Hero";
+import { Outlet } from "react-router";
 
 function App() {
-  const [search, setSearch] = useState<string>("");
-  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
-  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServorError] = useState<string>("");
-  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    console.log(e);
-  };
-
-  const onPortfolioCreate = (e: any) => {
-    e.preventDefault();
-    const exists = portfolioValues.find((value) => value === e.target[0].value);
-    const updatedPortfolio = [...portfolioValues, e.target[0].value];
-    if (exists) return;
-    setPortfolioValues(updatedPortfolio);
-  };
-
-  const onPortfolioDelete = (e: any) => {
-    e.preventDefault();
-    const removed = portfolioValues.filter((value) => {
-      return value !== e.target[0].value;
-    });
-    setPortfolioValues(removed);
-  };
-
-  const onSearchSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    const result = await searchCompanies(search);
-    if (typeof result === "string") {
-      setServorError(result);
-    } else if (Array.isArray(result.data)) {
-      setSearchResult(result.data);
-    }
-    console.log(searchResult);
-  };
-
   return (
-    <div className="App">
+    <>
       <Navbar />
-      <Search
-        onSearchSubmit={onSearchSubmit}
-        search={search}
-        handleSearchChange={handleSearchChange}
-      />
-      {serverError && <h1>{serverError}</h1>}
-      <ListPortfolio
-        portfolioValues={portfolioValues}
-        onPortfolioDelete={onPortfolioDelete}
-      />
-      <CardList
-        searchResult={searchResult}
-        onPortfolioCreate={onPortfolioCreate}
-      />
-    </div>
+      <Outlet />
+    </>
   );
 }
 
--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.png";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 interface Props {}
 
@@ -8,12 +9,14 @@ const Navbar = (props: Props) => {
   return (
     <nav className="relative container mx-auto p-6">
       <div className="flex items-center justify-between">
-        <div className="flex items-center space-x-20">
-          <img src={logo} alt="" />
+        <div className="flex items-center space-x-2s0">
+          <Link to="/">
+            <img src={logo} alt="" />
+          </Link>
           <div className="hidden font-bold lg:flex">
-            <a href="" className="text-black hover:text-darkBlue">
-              Dashboard
-            </a>
+            <Link to="/search" className="text-black hover:text-darkBlue">
+              Search
+            </Link>
           </div>
         </div>
         <div className="hidden lg:flex items-center space-x-6 text-back">
